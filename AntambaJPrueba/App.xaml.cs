ba7b70bb35c6e0aa8cf372f650055231d1630c72
laboratorio <laboratorio@UPOAULA10702>
2025-06-05 07:19:21 -0500

    Agregar archivos de proyecto.

---
+++ AntambaJPrueba/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AntambaJPrueba
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AntambaJPrueba/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace AntambaJPrueba;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
